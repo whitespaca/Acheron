347938bc1ec6a2f726081d192b52df578ea68fde
whitespaca <160763729+whitespaca@users.noreply.github.com>
2024-02-22 01:41:07 +0900

    Update main.py

--- builder/main.py
+++ builder/main.py
@@ -14,9 +14,9 @@ from util.writeconfig import WriteConfig
 
 def main():
     stars = requests.get(
-        f"https://api.github.com/repos/HELLODFDS/Acheron-dos-tool").json()["stargazers_count"]
+        f"https://api.github.com/repos/whitespaca/Acheron").json()["stargazers_count"]
     forks = requests.get(
-        f"https://api.github.com/repos/HELLODFDS/Acheron-dos-tool").json()["forks_count"]
+        f"https://api.github.com/repos/whitespaca/Acheron").json()["forks_count"]
 
     logging.basicConfig(
         level="NOTSET",
